d8958220f00d84a820376cab3fb10f41dc759f93
Kalaimakan06 <kalaimakan1510@gmail.com>
2025-07-17 20:25:16 +0530

    Add project files.

---
+++ CarRental/Database/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CarRental.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRental.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Car> Cars { get; set; }
+    }
+}
---
+++ CarRental/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿namespace CarRental.Models
+{
+    public class Car
+    {
+        public Guid Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Colour { get; set; }
+        public string Image { get; set; }
+        public decimal PricePerDay { get; set; }
+        public string Status { get; set; }
+        public string Description { get; set; }
+        
+    }
+}
